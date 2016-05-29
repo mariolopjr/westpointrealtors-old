043d17cd741b469b76d6a20db647054b9c642ffb
Mario <mariolopjr@icloud.com>
2016-05-28 21:56:02 -0400

    More adaptations to new bootstrap: nav fixes, centered main content container

--- header.php
+++ header.php
@@ -12,31 +12,29 @@ $applicationVersion = techmunchies\functions\loadData ( TBL_SETTINGS, "applicati
 
 <nav class="navbar navbar-full navbar-fixed-top">
     <div style="transform: matrix(1, 0, 0, 1, 0, -62);"></div>
-    <div class="siteNav">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/"><?=$applicationName?></a>
-        </div>
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="/">Home</a></li>
-                <?=$adminMenuItem?>
-                <li><a href="" data-toggle="modal" data-target="#aboutModal">About</a></li>
-                <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account</a>
-                <ul class="dropdown-menu">
-                    <ul class="list-inline">
-                        <li><a href="/settings.php">Settings</a></li>
-                        <li><a href="/logout.php">Logout</a></li>
-                    </ul>
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/"><?=$applicationName?></a>
+    </div>
+    <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="/">Home</a></li>
+            <?=$adminMenuItem?>
+            <li><a href="" data-toggle="modal" data-target="#aboutModal">About</a></li>
+            <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account</a>
+            <ul class="dropdown-menu">
+                <ul class="list-inline">
+                    <li><a href="/settings.php">Settings</a></li>
+                    <li><a href="/logout.php">Logout</a></li>
                 </ul>
-                </li>
             </ul>
-        </div>
+            </li>
+        </ul>
     </div>
 </nav>
--- index.php
+++ index.php
@@ -29,87 +29,84 @@ $pageTitle = "$applicationName - Main";
 
 </head>
 <body>
-<!-- Main Container  -->
-<div class="container">
+<!-- Site Header  -->
+<?php include "header.php"; ?>
+<!-- /Site Header -->
 
-    <!-- Site Header  -->
-    <?php include "header.php"; ?>
-    <!-- /Site Header -->
+<div class="container container-center">
+<!-- House Search Block  -->
+<div class="houseSearchBlock">
+    animatron + search goes here
+<!-- /House Search Block -->
 
-    <!-- House Search Block  -->
-    <div class="houseSearchBlock">
-        animatron + search goes here
-    <!-- /House Search Block -->
-
-    <!-- Favorite Listings  -->
-    <div class="favoriteListings album text-muted">
-        <div class="row">
-            <div class="favoriteHomeContainer card">
-                <img src="http://placehold.it/350x300" style="height: 300px; width: 100%; display: block;" alt="Beautiful Home 1" />
-                <p class="home-card-text">
-                    Beautiful home 1.......<br />
-                    12345 NW 3rd St, Orlando, FL 32816<br />
-                    $150,000<br />
-                </p>
-            </div>
-            <div class="favoriteHomeContainer card">
-                <img src="http://placehold.it/350x300" style="height: 300px; width: 100%; display: block;" alt="Beautiful Home 2" />
-                <p class="home-card-text">
-                    Beautiful home 2.......<br />
-                    12345 NW 3rd St, Orlando, FL 32816<br />
-                    $150,000<br />
-                </p>
-            </div>
-            <div class="favoriteHomeContainer card">
-                <img src="http://placehold.it/350x300" style="height: 300px; width: 100%; display: block;" alt="Beautiful Home 3" />
-                <p class="home-card-text">
-                    Beautiful home 3.......<br />
-                    12345 NW 3rd St, Orlando, FL 32816<br />
-                    $150,000<br />
-                </p>
-            </div>
-            <div class="favoriteHomeContainer card">
-                <img src="http://placehold.it/350x300" style="height: 300px; width: 100%; display: block;" alt="Beautiful Home 4" />
-                <p class="home-card-text">
-                    Beautiful home 4.......<br />
-                    12345 NW 3rd St, Orlando, FL 32816<br />
-                    $150,000<br />
-                </p>
-            </div>
-            <div class="favoriteHomeContainer card">
-                <img src="http://placehold.it/350x300" style="height: 300px; width: 100%; display: block;" alt="Beautiful Home 5" />
-                <p class="home-card-text">
-                    Beautiful home 5.......<br />
-                    12345 NW 3rd St, Orlando, FL 32816<br />
-                    $150,000<br />
-                </p>
-            </div>
-            <div class="favoriteHomeContainer card">
-                <img src="http://placehold.it/350x300" style="height: 300px; width: 100%; display: block;" alt="Beautiful Home 6" />
-                <p class="home-card-text">
-                    Beautiful home 6.......<br />
-                    12345 NW 3rd St, Orlando, FL 32816<br />
-                    $150,000<br />
-                </p>
-            </div>
+<!-- Favorite Listings  -->
+<div class="favoriteListings album text-muted">
+    <div class="row">
+        <div class="favoriteHomeContainer card">
+            <img src="http://placehold.it/350x300" style="height: 300px; width: 100%; display: block;" alt="Beautiful Home 1" />
+            <p class="home-card-text">
+                Beautiful home 1.......<br />
+                12345 NW 3rd St, Orlando, FL 32816<br />
+                $150,000<br />
+            </p>
+        </div>
+        <div class="favoriteHomeContainer card">
+            <img src="http://placehold.it/350x300" style="height: 300px; width: 100%; display: block;" alt="Beautiful Home 2" />
+            <p class="home-card-text">
+                Beautiful home 2.......<br />
+                12345 NW 3rd St, Orlando, FL 32816<br />
+                $150,000<br />
+            </p>
+        </div>
+        <div class="favoriteHomeContainer card">
+            <img src="http://placehold.it/350x300" style="height: 300px; width: 100%; display: block;" alt="Beautiful Home 3" />
+            <p class="home-card-text">
+                Beautiful home 3.......<br />
+                12345 NW 3rd St, Orlando, FL 32816<br />
+                $150,000<br />
+            </p>
+        </div>
+        <div class="favoriteHomeContainer card">
+            <img src="http://placehold.it/350x300" style="height: 300px; width: 100%; display: block;" alt="Beautiful Home 4" />
+            <p class="home-card-text">
+                Beautiful home 4.......<br />
+                12345 NW 3rd St, Orlando, FL 32816<br />
+                $150,000<br />
+            </p>
+        </div>
+        <div class="favoriteHomeContainer card">
+            <img src="http://placehold.it/350x300" style="height: 300px; width: 100%; display: block;" alt="Beautiful Home 5" />
+            <p class="home-card-text">
+                Beautiful home 5.......<br />
+                12345 NW 3rd St, Orlando, FL 32816<br />
+                $150,000<br />
+            </p>
+        </div>
+        <div class="favoriteHomeContainer card">
+            <img src="http://placehold.it/350x300" style="height: 300px; width: 100%; display: block;" alt="Beautiful Home 6" />
+            <p class="home-card-text">
+                Beautiful home 6.......<br />
+                12345 NW 3rd St, Orlando, FL 32816<br />
+                $150,000<br />
+            </p>
         </div>
     </div>
-    <!-- /Favorite Listings -->
-
-    <!-- Footer and Contact Block  -->
-    <div class="footerContactBlock">
-        <!-- Load footer -->
-        <?php include "footer.php"; ?>
-    </div>
-    <!-- /Footer and Contact Block -->
-
-    <!-- To Site Top  -->
-    <a href="#" class="toSiteTop hidden">
-        <i class="fa fa-angle-up toSiteTopHover" aria-hidden="true"></i>
-    </a>
-    <!-- /To Site Top -->
 </div>
-<!-- /Main Container -->
+<!-- /Favorite Listings -->
+</div>
+
+<!-- Footer and Contact Block  -->
+<div class="footerContactBlock">
+    <!-- Load footer -->
+    <?php include "footer.php"; ?>
+</div>
+<!-- /Footer and Contact Block -->
+
+<!-- To Site Top  -->
+<a href="#" class="toSiteTop hidden">
+    <i class="fa fa-angle-up toSiteTopHover" aria-hidden="true"></i>
+</a>
+<!-- /To Site Top -->
 
 <!-- Site JavaScripts  -->
 <?php include "includes/scripts.html"; ?>
