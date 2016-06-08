f26c84f06cc041827dc0391fc03ce45091dfa915
Mario <mariolopjr@icloud.com>
2016-06-08 14:22:41 -0400

    Huge cleanup, removed all files being referenced by CDN, loading CSS with PHP as well

---
+++ includes/css.php
@@ -0,0 +1,8 @@
+<!-- Bootstrap -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.2/css/bootstrap.min.css">
+
+<!-- FontAwesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+<!-- Custom CSS -->
+<link rel="stylesheet" href="css/theme.css">
--- index.php
+++ index.php
@@ -13,19 +13,9 @@ $pageTitle = "$applicationName - Main";
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?=$pageTitle?></title>
 
-<!-- Bootstrap -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.2/css/bootstrap.min.css">
-<link rel="stylesheet" href="css/theme.css">
-
-<!-- FontAwesome -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
+<!-- Site CSS  -->
+<?php require_once WEB_ROOT . "includes/css.php"; ?>
+<!-- /Site CSS -->
 
 </head>
 <body>
@@ -108,9 +98,9 @@ $pageTitle = "$applicationName - Main";
 </a>
 <!-- /To Site Top -->
 
-<!-- Site JavaScripts  -->
-<?php include "includes/scripts.html"; ?>
-<!-- /Site JavaScripts -->
+<!-- Site JavaScript  -->
+<?php require_once WEB_ROOT . "includes/js.php"; ?>
+<!-- /Site JavaScript -->
 
 </body>
 </html>
