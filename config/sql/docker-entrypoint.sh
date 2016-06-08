a9255082b68431030dabc7802443887eb49a9546
Mario <mariolopjr@icloud.com>
2016-06-08 14:07:56 -0400

    SQL Dockerfile modified to install required packages only, on-the-fly template gen relegated to entrypoint

--- config/sql/docker-entrypoint.sh
+++ config/sql/docker-entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Install Cron
-apt-get update
-apt-get install -y cron gettext
-
 # Export username, password, and output for SQL backup
 envsubst '$MYSQL_DB_NAME $MYSQL_DB_USERNAME $MYSQL_ROOT_PASSWORD $MYSQL_DB_BACKUP_FILE_PATH' < /backup.template > /etc/cron.daily/backup
 chmod ug+x /etc/cron.daily/backup
