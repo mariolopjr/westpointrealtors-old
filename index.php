71a124c6ff1be042d214acb100c9490bbb748124
Mario <mariolopjr@icloud.com>
2016-06-10 14:46:01 -0400

    Modifying popovers to be compatible with auto-generated JS

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ $pageTitle = "$applicationName - Main";
     <div class="row">
         <a href="#">
             <div id="house1" class="card" data-toggle="popover">
-                <img data-src="holder.js/100px280/thumb" alt="Beautiful Home" data-holder-rendered="true">
+                <img data-src="holder.js/100px280?text=House 1" alt="Beautiful Home" data-holder-rendered="true">
                 <p>
                     <div class="house-price">
                         $150,000
@@ -55,8 +55,8 @@ $pageTitle = "$applicationName - Main";
             </div>
         </a>
         <a href="#">
-            <div id="house1" class="card" data-toggle="popover">
-                <img data-src="holder.js/100px280/thumb" alt="Beautiful Home" data-holder-rendered="true">
+            <div id="house2" class="card" data-toggle="popover">
+                <img data-src="holder.js/100px280?text=House 2" alt="Beautiful Home" data-holder-rendered="true">
                 <p>
                     <div class="house-price">
                         $150,000
@@ -71,8 +71,8 @@ $pageTitle = "$applicationName - Main";
             </div>
         </a>
         <a href="#">
-            <div id="house1" class="card" data-toggle="popover">
-                <img data-src="holder.js/100px280/thumb" alt="Beautiful Home" data-holder-rendered="true">
+            <div id="house3" class="card" data-toggle="popover">
+                <img data-src="holder.js/100px280?text=House 3" alt="Beautiful Home" data-holder-rendered="true">
                 <p>
                     <div class="house-price">
                         $150,000
@@ -87,8 +87,8 @@ $pageTitle = "$applicationName - Main";
             </div>
         </a>
         <a href="#">
-            <div id="house1" class="card" data-toggle="popover">
-                <img data-src="holder.js/100px280/thumb" alt="Beautiful Home" data-holder-rendered="true">
+            <div id="house4" class="card" data-toggle="popover">
+                <img data-src="holder.js/100px280?text=House 4" alt="Beautiful Home" data-holder-rendered="true">
                 <p>
                     <div class="house-price">
                         $150,000
@@ -103,8 +103,8 @@ $pageTitle = "$applicationName - Main";
             </div>
         </a>
         <a href="#">
-            <div id="house1" class="card" data-toggle="popover">
-                <img data-src="holder.js/100px280/thumb" alt="Beautiful Home" data-holder-rendered="true">
+            <div id="house5" class="card" data-toggle="popover">
+                <img data-src="holder.js/100px280?text=House 5" alt="Beautiful Home" data-holder-rendered="true">
                 <p>
                     <div class="house-price">
                         $150,000
@@ -119,8 +119,8 @@ $pageTitle = "$applicationName - Main";
             </div>
         </a>
         <a href="#">
-            <div id="house1" class="card" data-toggle="popover">
-                <img data-src="holder.js/100px280/thumb" alt="Beautiful Home" data-holder-rendered="true">
+            <div id="house6" class="card" data-toggle="popover">
+                <img data-src="holder.js/100px280?text=House 6" alt="Beautiful Home" data-holder-rendered="true">
                 <p>
                     <div class="house-price">
                         $150,000
@@ -156,7 +156,61 @@ $pageTitle = "$applicationName - Main";
 <!-- /Site JavaScript -->
 <script>
 $(document).ready(function() {
+
+    function hidePopover(e) {
+        $(e).popover('hide');
+    }
+
+    // Sets up the popover defaults
+    $('.card').popover({
+        trigger: 'manual',
+        placement: 'top',
+        template: '<div class="popover popover-card" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-title text-xs-center"></h3><div class="popover-content"></div></div>',
+        //offset: 15,
+        delay: { show: 350, hide: 100 },
+        html: true
+    });
+
+    var timer, parentPopover;
+
+    $('.card').hover(function() {
+        var self = this;
+        clearTimeout(timer);
+        $('.popover').hide(); //Hide any open popovers on other elements.
+        parentPopover = self
+        $(self).popover('show');
+    },
+    function() {
+        var self = this;
+        timer = setTimeout(function(){ hidePopover(self) }, 300);
+    });
+
+    $('.popover').live({
+        mouseover: function() {
+            clearTimeout(timer);
+        },
+        mouseleave: function() {
+            var self = this;
+            timer = setTimeout(function(){ hidePopover(parentPopover) }, 300);
+        }
+    });
+
     $('#house1').popover({
+        title: '12345 NW 3rd St, Orlando, FL 32816',
+        content:
+            '<h3>' +
+            '<div>' +
+            '<img class="popover-image-small" data-src="holder.js/100px280" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280?text=+5" alt="Beautiful Home">' +
+            '</div>' +
+            '<hr>' +
+            '</h3>'
+    });
+
+    $('#house2').popover({
         trigger: "hover",
         placement: "top",
         template: '<div class="popover popover-card" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-title text-xs-center"></h3><div class="popover-content"></div></div>',
@@ -164,11 +218,87 @@ $(document).ready(function() {
         content:
             '<h3>' +
             '<div>' +
-            '<img class="popover-image-small" data-src="holder.js/100px280/thumb" alt="Beautiful Home" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22356%22%20height%3D%22280%22%20viewBox%3D%220%200%20356%20280%22%20preserveAspectRatio%3D%22none%22%3E%3C!--%0ASource%20URL%3A%20holder.js%2F100px280%2Fthumb%0ACreated%20with%20Holder.js%202.8.2.%0ALearn%20more%20at%20http%3A%2F%2Fholderjs.com%0A(c)%202012-2015%20Ivan%20Malopinsky%20-%20http%3A%2F%2Fimsky.co%0A--%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%3C!%5BCDATA%5B%23holder_15532de443c%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A18pt%20%7D%20%5D%5D%3E%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15532de443c%22%3E%3Crect%20width%3D%22356%22%20height%3D%22280%22%20fill%3D%22%23EEEEEE%22%2F%3E%3Cg%3E%3Ctext%20x%3D%22131.28125%22%20y%3D%22148.0484375%22%3E356x280%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">' +
-            '<img class="popover-image-small" data-src="holder.js/100px280/thumb" alt="Beautiful Home" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22356%22%20height%3D%22280%22%20viewBox%3D%220%200%20356%20280%22%20preserveAspectRatio%3D%22none%22%3E%3C!--%0ASource%20URL%3A%20holder.js%2F100px280%2Fthumb%0ACreated%20with%20Holder.js%202.8.2.%0ALearn%20more%20at%20http%3A%2F%2Fholderjs.com%0A(c)%202012-2015%20Ivan%20Malopinsky%20-%20http%3A%2F%2Fimsky.co%0A--%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%3C!%5BCDATA%5B%23holder_15532de443c%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A18pt%20%7D%20%5D%5D%3E%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15532de443c%22%3E%3Crect%20width%3D%22356%22%20height%3D%22280%22%20fill%3D%22%23EEEEEE%22%2F%3E%3Cg%3E%3Ctext%20x%3D%22131.28125%22%20y%3D%22148.0484375%22%3E356x280%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">' +
-            '<img class="popover-image-small" data-src="holder.js/100px280/thumb" alt="Beautiful Home" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22356%22%20height%3D%22280%22%20viewBox%3D%220%200%20356%20280%22%20preserveAspectRatio%3D%22none%22%3E%3C!--%0ASource%20URL%3A%20holder.js%2F100px280%2Fthumb%0ACreated%20with%20Holder.js%202.8.2.%0ALearn%20more%20at%20http%3A%2F%2Fholderjs.com%0A(c)%202012-2015%20Ivan%20Malopinsky%20-%20http%3A%2F%2Fimsky.co%0A--%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%3C!%5BCDATA%5B%23holder_15532de443c%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A18pt%20%7D%20%5D%5D%3E%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15532de443c%22%3E%3Crect%20width%3D%22356%22%20height%3D%22280%22%20fill%3D%22%23EEEEEE%22%2F%3E%3Cg%3E%3Ctext%20x%3D%22131.28125%22%20y%3D%22148.0484375%22%3E356x280%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">' +
-            '<img class="popover-image-small" data-src="holder.js/100px280/thumb" alt="Beautiful Home" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22356%22%20height%3D%22280%22%20viewBox%3D%220%200%20356%20280%22%20preserveAspectRatio%3D%22none%22%3E%3C!--%0ASource%20URL%3A%20holder.js%2F100px280%2Fthumb%0ACreated%20with%20Holder.js%202.8.2.%0ALearn%20more%20at%20http%3A%2F%2Fholderjs.com%0A(c)%202012-2015%20Ivan%20Malopinsky%20-%20http%3A%2F%2Fimsky.co%0A--%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%3C!%5BCDATA%5B%23holder_15532de443c%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A18pt%20%7D%20%5D%5D%3E%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15532de443c%22%3E%3Crect%20width%3D%22356%22%20height%3D%22280%22%20fill%3D%22%23EEEEEE%22%2F%3E%3Cg%3E%3Ctext%20x%3D%22131.28125%22%20y%3D%22148.0484375%22%3E356x280%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">' +
-            '<img class="popover-image-small" data-src="holder.js/100px280/thumb" alt="Beautiful Home" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22356%22%20height%3D%22280%22%20viewBox%3D%220%200%20356%20280%22%20preserveAspectRatio%3D%22none%22%3E%3C!--%0ASource%20URL%3A%20holder.js%2F100px280%2Fthumb%0ACreated%20with%20Holder.js%202.8.2.%0ALearn%20more%20at%20http%3A%2F%2Fholderjs.com%0A(c)%202012-2015%20Ivan%20Malopinsky%20-%20http%3A%2F%2Fimsky.co%0A--%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%3C!%5BCDATA%5B%23holder_15532de443c%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A18pt%20%7D%20%5D%5D%3E%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15532de443c%22%3E%3Crect%20width%3D%22356%22%20height%3D%22280%22%20fill%3D%22%23EEEEEE%22%2F%3E%3Cg%3E%3Ctext%20x%3D%22131.28125%22%20y%3D%22148.0484375%22%3E356x280%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" alt="Beautiful Home">' +
+            '</div>' +
+            '<hr>' +
+            '</h3>',
+        html: true
+    });
+
+    $('#house3').popover({
+        trigger: "hover",
+        placement: "top",
+        template: '<div class="popover popover-card" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-title text-xs-center"></h3><div class="popover-content"></div></div>',
+        title: '12345 NW 3rd St, Orlando, FL 32816',
+        content:
+            '<h3>' +
+            '<div>' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" alt="Beautiful Home">' +
+            '</div>' +
+            '<hr>' +
+            '</h3>',
+        html: true
+    });
+
+    $('#house4').popover({
+        trigger: "hover",
+        placement: "top",
+        template: '<div class="popover popover-card" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-title text-xs-center"></h3><div class="popover-content"></div></div>',
+        title: '12345 NW 3rd St, Orlando, FL 32816',
+        content:
+            '<h3>' +
+            '<div>' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" alt="Beautiful Home">' +
+            '</div>' +
+            '<hr>' +
+            '</h3>',
+        html: true
+    });
+
+    $('#house5').popover({
+        trigger: "hover",
+        placement: "top",
+        template: '<div class="popover popover-card" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-title text-xs-center"></h3><div class="popover-content"></div></div>',
+        title: '12345 NW 3rd St, Orlando, FL 32816',
+        content:
+            '<h3>' +
+            '<div>' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" alt="Beautiful Home">' +
+            '</div>' +
+            '<hr>' +
+            '</h3>',
+        html: true
+    });
+
+    $('#house6').popover({
+        trigger: "hover",
+        placement: "top",
+        template: '<div class="popover popover-card" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-title text-xs-center"></h3><div class="popover-content"></div></div>',
+        title: '12345 NW 3rd St, Orlando, FL 32816',
+        content:
+            '<h3>' +
+            '<div>' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" data-src="holder.js/100x280/thumb" alt="Beautiful Home" data-holder-rendered="true">' +
+            '<img class="popover-image-small" alt="Beautiful Home">' +
             '</div>' +
             '<hr>' +
             '</h3>',
