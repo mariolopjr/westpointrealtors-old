c82b6c34859e9c059e780c294e6a5d2fd3005a6e
Mario <mariolopjr@icloud.com>
2016-06-16 13:46:55 -0400

    Changed slogan to lowercase

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <p>Copyright <?=$applicationName?></p>
     <p>
-    <p>Site designed and hosted by <a href="http://techmunchies.net">techmunchies - Take a byte</a></p>
+    <p>Site designed and hosted by <a href="http://techmunchies.net">techmunchies - take a byte</a></p>
     </p>
 </footer>
