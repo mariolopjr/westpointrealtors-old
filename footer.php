48abe33ec0ab18ba87885bd7c5ba99eec5a2b436
Mario <mariolopjr@icloud.com>
2016-06-18 15:14:00 -0400

    Changed footer url a for self-promotion

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <p>Copyright <?=$applicationName?></p>
     <p>
-    <p>Site designed and hosted by <a href="http://techmunchies.net">techmunchies - take a byte</a></p>
+    <p>Site designed by <a href="http://techmunchies.net">techmunchies - take a byte</a></p>
     </p>
 </footer>
