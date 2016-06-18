a4014355ae2153aba15f04024fd7957288dc1b7b
Mario <mariolopjr@icloud.com>
2016-06-18 15:14:19 -0400

    Added Piwik analytics tracking code

--- includes/js.php
+++ includes/js.php
@@ -24,3 +24,19 @@
 
 <!-- Index JS -->
 <script src="../js/index.js"></script>
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//stats.techmunchies.net/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 2]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//stats.techmunchies.net/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
